557cab71a9e147d77b4f8f0cf15a71cdd91ff6a0
Matthew <mharyant@students.cs.ubc.ca>
2024-01-20 13:04:39 -0800

    Initialize google maps api

---
+++ front_end/src/components/Map.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+
+const center = {
+  lat: 49.2625556626212,
+  lng: -123.24458473892533
+};
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'b7404d74019955c3',
+    googleMapsApiKey: "AIzaSyCejmTIDG6EX0omAii1llZgdy_nKMemAws"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(Map)
