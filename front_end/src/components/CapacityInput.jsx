30095cca533a1813a1504e766d64ee8354912343
Sean Louie <sean@dhcp-206-87-204-84.eduroam.wireless.ubc.ca>
2024-01-20 14:46:58 -0800

    pushing popup

---
+++ front_end/src/components/CapacityInput.jsx
@@ -0,0 +1,14 @@
+import { Input } from '@chakra-ui/react'
+
+function CapacityInput(){
+    return(
+        <>
+        <h2>
+            Enter Capacity:
+        </h2>
+        <Input placeholder='Please enter capacity' />
+        </>
+    )
+}
+
+export default CapacityInput
